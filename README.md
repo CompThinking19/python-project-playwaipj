d67ab7d052c00e7670ec00e0d64c7f13a5bef85e
pj <pjw98@scarletmail.rutgers.edu>
2019-11-19 10:59:16 -0500

    2nd mile

---
+++ README.md
@@ -0,0 +1,30 @@
+# ThoughtBot
+
+- This project will be using randomization to generate thoughts. Most of the generated thoughts may not make sense, but that's not different from how thoughts work in reality.
+
+- Currently the random thoughts are generated using datasets stored using json files with adjectives from `adjectives.json` and animals from `animals.json` found on the internet, I will try to switch some things up to make the thoughts more interesting and realistic.
+
+- I've also added some TODO tasks using the `todo.json` file.
+
+- Using quotes found online. Stored in `quotes.json`.
+
+- I might be able to make a website for this project. I'll need to learn some javascript for that first.
+
+### TODO
+
+- Add more entries to the `todo.json` file.
+
+- Sometimes the random thoughts will be same as the previous thought. Find a way to change that.
+
+### Note
+
+- This project uses f-Strings which is a part of Python 3.6 and above. Please use the appropriate version to run the `tb.py` file. The command `python3.6 tb.py` works well on clamshell.
+
+- The screen width should be atleast `42` ( [Answer to the Ultimate Question of Life, the Universe, and Everything](https://en.wikipedia.org/wiki/42_%28number%29#The_Hitchhiker%27s_Guide_to_the_Galaxy) ) characters wide to display the starting text successfully.
+
+### Sources
+
+- animals.json    https://gist.github.com/borlaym/585e2e09dd6abd9b0d0a
+- adjectives.json https://github.com/moby/moby/blob/master/pkg/namesgenerator/names-generator.go
+- quotes.json     https://gist.github.com/dmakk767/9375ff01aff76f1788aead1df9a66338
+- figlet          http://www.figlet.org/
---
+++ tb.py
@@ -0,0 +1,67 @@
+import json
+import time
+import random
+
+# This is our thinker function which utilizes the random module to return a string to the while loop.
+def thinker():
+    r = random.randint(0, 10)
+
+    if r < 4:
+        adjective = random.choice(adjectives)
+        # Logic to determine the usage of `A` or `An`.
+        article = 'A'
+        if adjective[0] in "aeiouAEIOU":
+            article += 'n'
+        return f"{ article } { adjective } { random.choice(animals) }"
+    if r < 7:
+        quote = random.choice(quotes)
+        return f"{ quote['quote'] } - { quote['name'] } "
+
+    return f"{ random.choice(todo) }"
+
+# We import our datasets below. Sources are provided before each `with` statement.
+
+# https://gist.github.com/borlaym/585e2e09dd6abd9b0d0a
+with open('animals.json', 'r') as animals_file:
+    animals = json.loads(animals_file.read())
+
+# https://github.com/moby/moby/blob/master/pkg/namesgenerator/names-generator.go
+with open('adjectives.json', 'r') as adjectives_file:
+    adjectives = json.loads(adjectives_file.read())
+
+# https://gist.github.com/dmakk767/9375ff01aff76f1788aead1df9a66338
+with open('quotes.json', 'r') as quotes_file:
+    quotes = json.loads(quotes_file.read())
+
+with open('todo.json', 'r') as todo_file:
+    todo = json.loads(todo_file.read())
+
+# Using figlet for fancy printing. Screen should be wide enought to format this correctly...
+
+# http://www.figlet.org/
+print("""
+==========================================
+  _   _                       _     _
+ | |_| |__   ___  _   _  __ _| |__ | |_
+ | __| '_ \ / _ \| | | |/ _` | '_ \| __|
+ | |_| | | | (_) | |_| | (_| | | | | |_
+  \__|_| |_|\___/ \__,_|\__, |_| |_|\__|
+                         |___/
+
+             |----|
+   O     O   |beep|     _           _
+   |_____|  /|boop|    | |__   ___ | |_
+   | _ _ | / |----|    | '_ \ / _ \| __|
+   |  _  |/            | |_) | (_) | |_
+   |_____|             |_.__/ \___/ \__|
+
+==========================================
+""")
+
+try:
+    while True:
+        print(thinker())
+        time.sleep(random.randint(0, 3))
+# Graceful termination
+except KeyboardInterrupt:
+    print("\nKeyboard Interrupt found. Exiting...")
